5e2be62539bc0ec01ac72d0a7a08ba9b5d9654de
kennyderek <kderek@mit.edu>
2019-01-28 16:09:53 +0100

    changed static url

--- essen/essen/settings.py
+++ essen/essen/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
-STATIC_URL = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_URL = "static"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
