2d6b695ce93439cbf17f9a8d4ae94471fd89d164
kennyderek <kderek@mit.edu>
2019-01-28 16:29:08 +0100

    changed static url

--- essen/essen/settings.py
+++ essen/essen/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = ""
 
 STATIC_URL = "static"
 
