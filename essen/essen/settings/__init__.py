37b0f05bbb0ee62f1924cd96a7fbc65912977e33
Nicolas Camenisch <garnele007@gmail.com>
2022-01-12 19:58:16 -0500

    Modularize settings for development and production

---
+++ essen/essen/settings/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+try:
+    from .base import *
+except ImportError:
+    pass
+
+
+senv = os.environ.get('DH_SETTINGS_ENVIRONMENT', 'dev')
+if senv == 'dev':
+    from .dev import *
+elif senv == 'prod':
+    from .prod import *
+else:
+    raise ImportError("Couldn't determine which settings environment to load. Make sure that the DH_SETTINGS_ENVIRONMENT environment variable is set correctly.")
---
+++ essen/essen/settings/base.py
@@ -0,0 +1,124 @@
+"""
+Base Settings file.
+"""
+
+import os
+from django.contrib.messages import constants as messages
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Application definition
+INSTALLED_APPS = [
+    'home.apps.HomeConfig',
+    'recipes.apps.RecipesConfig',
+    'menu.apps.MenuConfig',
+
+    'admin_interface',
+    'colorfield',
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    'widget_tweaks',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'mit.auth.KerbRemoteUserMiddleware',
+    'home.middleware.CreateProfileMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'mit.auth.KerbRemoteUserBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+ROOT_URLCONF = 'essen.urls'
+WSGI_APPLICATION = 'essen.wsgi.application'
+
+
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/essen'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Crispy Forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# Required by admin_interface
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+SILENCED_SYSTEM_CHECKS = ['security.W019']
+
+# Bootstrap Messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
