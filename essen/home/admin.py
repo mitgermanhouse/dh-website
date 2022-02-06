45453b28674c74fd9e9801928a870f62a3fa7620
Nicolas Camenisch <garnele007@gmail.com>
2022-02-06 15:32:53 -0500

    Improve ModelAdmin for the Member model

--- essen/home/admin.py
+++ essen/home/admin.py
@@ -2,9 +2,25 @@ from django.contrib import admin
 
 from home.models import Member, DietaryRestriction, GalleryContent
 
+@admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    search_fields = ['user__first_name', 'user__last_name']
 
-admin.site.register(Member, MemberAdmin)
+    search_fields = ('user__first_name', 'user__last_name')
+    list_display = ('display_first_name', 'display_last_name', 'display_is_active')
+    ordering = ('-user__is_active', 'user__first_name', 'user__last_name')
+
+    @admin.display(description='First Name', ordering = 'user__first_name')
+    def display_first_name(self, obj: Member):
+        return obj.user.first_name
+
+    @admin.display(description='Last Name', ordering = 'user__last_name')
+    def display_last_name(self, obj: Member):
+        return obj.user.last_name
+
+    @admin.display(description='Is Active', boolean = True, ordering = 'user__is_active')
+    def display_is_active(self, obj: Member):
+        return obj.user.is_active
+
+
 admin.site.register(DietaryRestriction)
 admin.site.register(GalleryContent)
